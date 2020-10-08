0ebaf1a5a33a06bf73a8a54853f1b8961bf4ac0f
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-10-07 17:14:14 -0700

    create login and logout function

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,9 +1,9 @@
-skip_before_action :authorized, only: [:create]
- 
+class Api::V1::AuthController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
   def create
     @user = User.find_by(username: user_login_params[:username])
-    #User#authenticate comes from BCrypt
-    if @user && @user.authenticate(user_login_params[:password])
+    if @user&.authenticate(user_login_params[:password])
       # encode token comes from ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
@@ -11,11 +11,10 @@ skip_before_action :authorized, only: [:create]
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
   end
- 
+
   private
- 
+
   def user_login_params
-    # params { user: {username: 'Chandler Bing', password: 'hi' } }
     params.require(:user).permit(:username, :password)
   end
 end

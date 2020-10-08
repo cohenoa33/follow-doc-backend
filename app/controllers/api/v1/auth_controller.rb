22625227a4f5d046daded108e863213dfc239346
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-10-07 17:38:02 -0700

    .

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::AuthController < ApplicationController
   def create
     @user = User.find_by(username: user_login_params[:username])
     if @user&.authenticate(user_login_params[:password])
-      # encode token comes from ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
     else

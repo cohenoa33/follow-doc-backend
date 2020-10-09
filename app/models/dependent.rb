31cad0161328e6c8bea427b795c1548bf6f90e75
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-10-08 22:03:33 -0700

    added create method for problem

--- app/models/dependent.rb
+++ app/models/dependent.rb
@@ -2,4 +2,6 @@ class Dependent < ApplicationRecord
   belongs_to :user
   has_many :problems
   has_many :comments, through: :problems, dependent: :destroy
+
+  validates :name, presence: true,  length: { minimum: 2 }
 end

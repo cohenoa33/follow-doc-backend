c606cfeb6c8e47bc5f6c43acfaa974a841d50845
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-10-12 21:04:45 -0700

    Add address attribute to Doctor serializer

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,3 +1,3 @@
 class DoctorSerializer < ActiveModel::Serializer
-  attributes :id, :name, :street, :city, :zipcode, :state, :info, :latitude, :longitude
+  attributes :id, :name, :street, :city, :zipcode, :state, :info, :latitude, :longitude, :address
 end
